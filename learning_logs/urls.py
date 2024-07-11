cf7d73338f477f3cff6a6aae8ee8acb00140a97a
Pedro Henrique <pedrobezerraout@gmail.com>
2024-07-11 11:04:11 -0300

    att topicos

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('topics', views.topics, name='topics'),
+    path('topics/<topic_id>/', views.topic, name='topic'),
 ]
