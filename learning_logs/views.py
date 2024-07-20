8c1386f75946a24d38a1512880135909ff0df26c
Pedro Henrique <pedrobezerraout@gmail.com>
2024-07-20 13:15:59 -0300

    att

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -39,4 +39,17 @@ def new_topic(request):
 
 def new_entry(request, topic_id):
     """Acrescenta uma nova entrada para um assunto em particular."""
-    t
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        # Nenhum dado submetido; cria um formlário em branco.
+        form = EntryForm()
+    else:
+        # Dados de 'POST' submetidos; processa os dados.
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('topic', args=[topic_id]))
+    context = {'topic': topic, 'form':form}
+    return render(request, 'learning_logs/new_entry.html', context)
