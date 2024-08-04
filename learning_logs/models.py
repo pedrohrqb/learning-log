0e8ed82ed327c8af3ff22bfb4a76a94d7c15774d
Pedro Henrique <pedrobezerraout@gmail.com>
2024-08-04 13:22:40 -0300

    definindo donos aos topicos

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -7,7 +7,7 @@ class Topic(models.Model):
     está aprendendo"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
-
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         """Devolve uma representação em string do modelo"""
